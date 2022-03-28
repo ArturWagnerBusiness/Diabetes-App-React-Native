81c429fd59561f45d00b1107ae59c8880e5c05cd
K1908454 <K1908454@kingston.ac.uk>
2022-03-28 10:50:51 +0100

    Wrapping App.js in the ItemProvider Will allow for system storage to be accessed throughout the app

--- app/App.js
+++ app/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ItemProvider } from "./contexts/ItemContext";
 import AddBloodSugarLevel from "./screens/AddBloodSugarLevel";
 import AddExercise from "./screens/AddExercise";
 import AddIssue from "./screens/AddIssue";
@@ -11,39 +12,41 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="View">
-        <Stack.Screen
-          name="View"
-          component={ViewRecords}
-          options={{ title: "My Record History" }}
-        />
-        <Stack.Screen
-          name="Meal"
-          component={AddMeal}
-          options={{ title: "Add New Meal" }}
-        />
-        <Stack.Screen
-          name="Medication"
-          component={AddMedication}
-          options={{ title: "Add New Medication" }}
-        />
-        <Stack.Screen
-          name="Issue"
-          component={AddIssue}
-          options={{ title: "Add New Issue/Challenge" }}
-        />
-        <Stack.Screen
-          name="BloodSugar"
-          component={AddBloodSugarLevel}
-          options={{ title: "Add New Blood Sugar Level" }}
-        />
-        <Stack.Screen
-          name="Exercise"
-          component={AddExercise}
-          options={{ title: "Add New Exercise" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ItemProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="View">
+          <Stack.Screen
+            name="View"
+            component={ViewRecords}
+            options={{ title: "My Record History" }}
+          />
+          <Stack.Screen
+            name="Meal"
+            component={AddMeal}
+            options={{ title: "Add New Meal" }}
+          />
+          <Stack.Screen
+            name="Medication"
+            component={AddMedication}
+            options={{ title: "Add New Medication" }}
+          />
+          <Stack.Screen
+            name="Issue"
+            component={AddIssue}
+            options={{ title: "Add New Issue/Challenge" }}
+          />
+          <Stack.Screen
+            name="BloodSugar"
+            component={AddBloodSugarLevel}
+            options={{ title: "Add New Blood Sugar Level" }}
+          />
+          <Stack.Screen
+            name="Exercise"
+            component={AddExercise}
+            options={{ title: "Add New Exercise" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ItemProvider>
   );
 }
